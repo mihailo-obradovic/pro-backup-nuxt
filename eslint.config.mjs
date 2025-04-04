3e480e039e57b2137533779adba9f183761b5e56
Mihailo Obradović <mihailo.obradovic.95@gmail.com>
2025-04-04 13:55:55 +0200

    Add Prettier, ESLint settings, force pnpm

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,34 @@
-// @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from './.nuxt/eslint.config.mjs';
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt({
+  rules: {
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'vue/component-name-in-template-casing': ['warn', 'PascalCase'],
+    'vue/html-self-closing': [
+      'warn',
+      {
+        html: {
+          void: 'always',
+          normal: 'always',
+          component: 'always'
+        }
+      }
+    ],
+    'vue/max-attributes-per-line': [
+      'warn',
+      {
+        singleline: {
+          max: 3
+        },
+        multiline: {
+          max: 1
+        }
+      }
+    ],
+    'vue/multi-word-component-names': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
+  }
+});
