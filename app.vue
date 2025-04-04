03a31d6922c811c693677fa0f68298f40adc6d9c
Mihailo Obradović <mihailo.obradovic.95@gmail.com>
2025-04-04 14:03:28 +0200

    Add base files, remove unnecessary ones

--- app.vue
+++ app.vue
@@ -1,6 +1,23 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtLoadingIndicator />
+
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
+
+<script setup>
+useHead({
+  title: 'Pro Backup Nuxt Test',
+
+  meta: [
+    { charset: 'utf-8' },
+    {
+      name: 'viewport',
+      content: 'width=device-width, initial-scale=1, shrink-to-fit=no'
+    }
+  ],
+
+  link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+});
+</script>
---
+++ error.vue
@@ -0,0 +1,18 @@
+<template>
+  <NuxtLoadingIndicator />
+
+  <NuxtLayout>
+    {{ error }}
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+import type { NuxtError } from '#app';
+
+defineProps({
+  error: {
+    type: Object as PropType<NuxtError>,
+    required: true
+  }
+});
+</script>
