94e4eb7eb35257a2b76273b4150c739b92f5a112
Mihailo Obradović <mihailo.obradovic.95@gmail.com>
2025-04-04 13:58:24 +0200

    Update Nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/test-utils', '@nuxt/icon']
-})
+
+  ssr: false,
+
+  runtimeConfig: {
+    app: {
+      //
+    },
+
+    public: {
+      //
+    }
+  },
+
+  modules: ['@nuxt/eslint', '@nuxt/test-utils', '@nuxt/icon'],
+
+  css: ['@/assets/styles/main.scss'],
+
+  components: {
+    dirs: ['@/components/shared']
+  },
+
+  imports: {
+    dirs: ['services']
+  }
+});
