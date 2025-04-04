46ab291f244bd51e522b6dc8c828924d1dbece94
Mihailo Obradović <mihailo.obradovic.95@gmail.com>
2025-04-04 16:05:51 +0200

    Add basic tabs

--- pages/Home.vue
+++ pages/Home.vue
@@ -1,5 +1,54 @@
 <template>
-  <h1>Hello, world!</h1>
+  <s-tabs v-model="activeTab">
+    <s-tabs-list class="flex w-full">
+      <s-tabs-trigger
+        v-for="tab in tabs"
+        :key="tab.value"
+        :value="tab.value"
+        class="flex-1"
+      >
+        <PContainer gap="2" class="items-center">
+          <PIcon :name="tab.icon" />
+          {{ tab.label }}
+        </PContainer>
+      </s-tabs-trigger>
+    </s-tabs-list>
+  </s-tabs>
 </template>
 
-<script setup></script>
+<script setup>
+const tabs = [
+  {
+    label: 'Tasks',
+    value: 'tasks',
+    icon: 'check-square'
+  },
+  {
+    label: 'Projects',
+    value: 'projects',
+    icon: 'folder'
+  },
+  {
+    label: 'Attachments',
+    value: 'attachments',
+    icon: 'paperclip'
+  },
+  {
+    label: 'Comments',
+    value: 'comments',
+    icon: 'message-square'
+  },
+  {
+    label: 'Custom fields',
+    value: 'custom-fields',
+    icon: 'grid'
+  },
+  {
+    label: 'Goals',
+    value: 'goals',
+    icon: 'target'
+  }
+];
+
+const activeTab = ref('tasks');
+</script>
