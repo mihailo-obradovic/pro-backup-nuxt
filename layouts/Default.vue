bc3c13c5bc39761dd09810d7e8f95343900587d7
Mihailo Obradović <mihailo.obradovic.95@gmail.com>
2025-04-04 15:46:21 +0200

    Use sidebar

--- layouts/Default.vue
+++ layouts/Default.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="p-4 h-full">
-    <slot />
-  </div>
+  <s-sidebar-provider>
+    <AppSidebar />
+
+    <main class="h-screen w-screen flex flex-col p-4">
+      <slot />
+    </main>
+  </s-sidebar-provider>
 </template>
 
 <script lang="ts" setup>
-//
+import AppSidebar from '@/components/AppSidebar.vue';
 </script>
