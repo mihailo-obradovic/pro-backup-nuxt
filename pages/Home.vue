7f3f3c8890458aa1870ad1ead32ce2c28597290d
Mihailo Obradović <mihailo.obradovic.95@gmail.com>
2025-04-07 12:28:42 +0200

    Add tabs

--- pages/Home.vue
+++ pages/Home.vue
@@ -1,9 +1,28 @@
 <template>
-  <v-container
-    class="fill-height d-flex flex-column align-center justify-center text-center"
-  >
-    <v-icon icon="mdi-home" size="64" />
-
-    <div class="mt-3">My Application's Home Page</div>
+  <v-container class="fill-height d-flex flex-column">
+    <v-tabs
+      v-model="activeTab"
+      bg-color="background"
+      align-tabs="start"
+      class="flex-grow-0"
+    >
+      <v-tab v-for="tab in tabs" :key="tab.name">
+        <v-icon>{{ tab.icon }}</v-icon>
+        {{ tab.name.charAt(0).toUpperCase() + tab.name.slice(1) }}
+      </v-tab>
+    </v-tabs>
   </v-container>
 </template>
+
+<script setup>
+const activeTab = ref('tasks');
+
+const tabs = [
+  { name: 'tasks', icon: 'mdi-check-circle' },
+  { name: 'projects', icon: 'mdi-folder' },
+  { name: 'attachments', icon: 'mdi-paperclip' },
+  { name: 'comments', icon: 'mdi-comment' },
+  { name: 'customFields', icon: 'mdi-form-textbox' },
+  { name: 'goals', icon: 'mdi-target' }
+];
+</script>
