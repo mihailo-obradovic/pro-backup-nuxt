b9ae9e9c7fe1b4851a50d622ecacb64410e41133
Mihailo Obradović <mihailo.obradovic.95@gmail.com>
2025-04-09 09:30:14 +0200

    Reorder

--- components/SampleTable.vue
+++ components/SampleTable.vue
@@ -1,3 +1,71 @@
+<template>
+  <div class="flex-1 divide-y divide-(--ui-border-accented) w-full">
+    <div class="flex items-center gap-2 px-4 py-3.5 overflow-x-auto">
+      <UInput
+        :model-value="
+          table?.tableApi?.getColumn('email')?.getFilterValue() as string
+        "
+        class="max-w-sm min-w-[12ch]"
+        placeholder="Filter emails..."
+        @update:model-value="
+          table?.tableApi?.getColumn('email')?.setFilterValue($event)
+        "
+      />
+
+      <UButton color="neutral" label="Randomize" @click="randomize" />
+
+      <UDropdownMenu
+        :items="
+          table?.tableApi
+            ?.getAllColumns()
+            .filter((column) => column.getCanHide())
+            .map((column) => ({
+              label: upperFirst(column.id),
+              type: 'checkbox' as const,
+              checked: column.getIsVisible(),
+              onUpdateChecked(checked: boolean) {
+                table?.tableApi
+                  ?.getColumn(column.id)
+                  ?.toggleVisibility(!!checked);
+              },
+              onSelect(e?: Event) {
+                e?.preventDefault();
+              }
+            }))
+        "
+        :content="{ align: 'end' }"
+      >
+        <UButton
+          label="Columns"
+          color="neutral"
+          variant="outline"
+          trailing-icon="i-lucide-chevron-down"
+          class="ml-auto"
+          aria-label="Columns select dropdown"
+        />
+      </UDropdownMenu>
+    </div>
+
+    <UTable
+      ref="table"
+      :data="data"
+      :columns="columns"
+      sticky
+      class="max-h-full"
+    >
+      <template #expanded="{ row }">
+        <pre>{{ row.original }}</pre>
+      </template>
+    </UTable>
+
+    <div class="px-4 py-3.5 text-sm text-(--ui-text-muted)">
+      {{ table?.tableApi?.getFilteredSelectedRowModel().rows.length || 0 }} of
+      {{ table?.tableApi?.getFilteredRowModel().rows.length || 0 }} row(s)
+      selected.
+    </div>
+  </div>
+</template>
+
 <script setup lang="ts">
 import { h, resolveComponent } from 'vue';
 import { upperFirst } from 'scule';
@@ -320,65 +388,3 @@ function randomize() {
   data.value = [...data.value].sort(() => Math.random() - 0.5);
 }
 </script>
-
-<template>
-  <div class="flex-1 divide-y divide-(--ui-border-accented) w-full">
-    <div class="flex items-center gap-2 px-4 py-3.5 overflow-x-auto">
-      <UInput
-        :model-value="
-          table?.tableApi?.getColumn('email')?.getFilterValue() as string
-        "
-        class="max-w-sm min-w-[12ch]"
-        placeholder="Filter emails..."
-        @update:model-value="
-          table?.tableApi?.getColumn('email')?.setFilterValue($event)
-        "
-      />
-
-      <UButton color="neutral" label="Randomize" @click="randomize" />
-
-      <UDropdownMenu
-        :items="
-          table?.tableApi
-            ?.getAllColumns()
-            .filter((column) => column.getCanHide())
-            .map((column) => ({
-              label: upperFirst(column.id),
-              type: 'checkbox' as const,
-              checked: column.getIsVisible(),
-              onUpdateChecked(checked: boolean) {
-                table?.tableApi
-                  ?.getColumn(column.id)
-                  ?.toggleVisibility(!!checked);
-              },
-              onSelect(e?: Event) {
-                e?.preventDefault();
-              }
-            }))
-        "
-        :content="{ align: 'end' }"
-      >
-        <UButton
-          label="Columns"
-          color="neutral"
-          variant="outline"
-          trailing-icon="i-lucide-chevron-down"
-          class="ml-auto"
-          aria-label="Columns select dropdown"
-        />
-      </UDropdownMenu>
-    </div>
-
-    <UTable ref="table" :data="data" :columns="columns" sticky class="h-96">
-      <template #expanded="{ row }">
-        <pre>{{ row.original }}</pre>
-      </template>
-    </UTable>
-
-    <div class="px-4 py-3.5 text-sm text-(--ui-text-muted)">
-      {{ table?.tableApi?.getFilteredSelectedRowModel().rows.length || 0 }} of
-      {{ table?.tableApi?.getFilteredRowModel().rows.length || 0 }} row(s)
-      selected.
-    </div>
-  </div>
-</template>
