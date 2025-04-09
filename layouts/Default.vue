4dc615f476396e546aff502479a6f14d79e020cd
Mihailo Obradović <mihailo.obradovic.95@gmail.com>
2025-04-09 08:48:55 +0200

    Add simple navbar

--- layouts/Default.vue
+++ layouts/Default.vue
@@ -1,184 +1,40 @@
 <template>
-  <UDashboardGroup unit="rem">
-    <UDashboardSidebar
-      id="default"
-      v-model:open="open"
-      collapsible
-      resizable
-      class="bg-(--ui-bg-elevated)/25"
-      :ui="{ footer: 'lg:border-t lg:border-(--ui-border)' }"
-    >
-      <template #header="{ collapsed }">
-        <TeamsMenu :collapsed="collapsed" />
+  <div>
+    <u-dashboard-navbar title="ProBackup">
+      <template #leading>
+        <u-dashboard-sidebar-collapse />
       </template>
 
-      <template #default="{ collapsed }">
-        <UDashboardSearchButton
-          :collapsed="collapsed"
-          class="bg-transparent ring-(--ui-border)"
-        />
-
-        <UNavigationMenu
-          :collapsed="collapsed"
-          :items="links[0]"
-          orientation="vertical"
-        />
-
-        <UNavigationMenu
-          :collapsed="collapsed"
-          :items="links[1]"
-          orientation="vertical"
-          class="mt-auto"
-        />
+      <template #trailing>
+        <u-badge label="4" variant="subtle" />
       </template>
 
-      <template #footer="{ collapsed }">
-        <UserMenu :collapsed="collapsed" />
+      <template #right>
+        <u-tabs
+          :items="items"
+          default-value="all"
+          size="sm"
+          class="w-40"
+          :content="false"
+        />
       </template>
-    </UDashboardSidebar>
+    </u-dashboard-navbar>
 
-    <UDashboardSearch :groups="groups" />
-
-    <slot />
-
-    <NotificationsSlideover />
-  </UDashboardGroup>
+    <main class="p-4">
+      <slot />
+    </main>
+  </div>
 </template>
 
 <script setup lang="ts">
-const route = useRoute();
-const toast = useToast();
-
-const open = ref(false);
-
-const links = [
-  [
-    {
-      label: 'Home',
-      icon: 'i-lucide-house',
-      to: '/',
-      onSelect: () => {
-        open.value = false;
-      }
-    },
-    {
-      label: 'Inbox',
-      icon: 'i-lucide-inbox',
-      to: '/inbox',
-      badge: '4',
-      onSelect: () => {
-        open.value = false;
-      }
-    },
-    {
-      label: 'Customers',
-      icon: 'i-lucide-users',
-      to: '/customers',
-      onSelect: () => {
-        open.value = false;
-      }
-    },
-    {
-      label: 'Settings',
-      to: '/settings',
-      icon: 'i-lucide-settings',
-      defaultOpen: true,
-      children: [
-        {
-          label: 'General',
-          to: '/settings',
-          exact: true,
-          onSelect: () => {
-            open.value = false;
-          }
-        },
-        {
-          label: 'Members',
-          to: '/settings/members',
-          onSelect: () => {
-            open.value = false;
-          }
-        },
-        {
-          label: 'Notifications',
-          to: '/settings/notifications',
-          onSelect: () => {
-            open.value = false;
-          }
-        },
-        {
-          label: 'Security',
-          to: '/settings/security',
-          onSelect: () => {
-            open.value = false;
-          }
-        }
-      ]
-    }
-  ],
-  [
-    {
-      label: 'Feedback',
-      icon: 'i-lucide-message-circle',
-      to: 'https://github.com/nuxt-ui-pro/dashboard',
-      target: '_blank'
-    },
-    {
-      label: 'Help & Support',
-      icon: 'i-lucide-info',
-      to: 'https://github.com/nuxt/ui-pro',
-      target: '_blank'
-    }
-  ]
-];
-
-const groups = computed(() => [
+const items = [
   {
-    id: 'links',
-    label: 'Go to',
-    items: links.flat()
+    label: 'All',
+    value: 'all'
   },
   {
-    id: 'code',
-    label: 'Code',
-    items: [
-      {
-        id: 'source',
-        label: 'View page source',
-        icon: 'i-simple-icons-github',
-        to: `https://github.com/nuxt-ui-pro/dashboard/blob/main/app/pages${route.path === '/' ? '/index' : route.path}.vue`,
-        target: '_blank'
-      }
-    ]
+    label: 'Unread',
+    value: 'unread'
   }
-]);
-
-onMounted(async () => {
-  const cookie = useCookie('cookie-consent');
-  if (cookie.value === 'accepted') {
-    return;
-  }
-
-  toast.add({
-    title:
-      'We use first-party cookies to enhance your experience on our website.',
-    duration: 0,
-    close: false,
-    actions: [
-      {
-        label: 'Accept',
-        color: 'neutral',
-        variant: 'outline',
-        onClick: () => {
-          cookie.value = 'accepted';
-        }
-      },
-      {
-        label: 'Opt out',
-        color: 'neutral',
-        variant: 'ghost'
-      }
-    ]
-  });
-});
+];
 </script>
